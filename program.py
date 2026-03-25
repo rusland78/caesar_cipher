f67bd878980bf6f41ff77d2c226dbf7b0b55bb80
rusland78 <r.vozhakin@gmail.com>
2026-03-26 00:29:38 +0300

    add code to crypt russian lowercase and uppercase text

--- program.py
+++ program.py
@@ -4,11 +4,17 @@ ru_low = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
 ru_up = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
 text = input('Введите ваш текст: ')
-shift = int(input('Шаг сдвига: '))
+key = int(input('Шаг сдвига: '))
 crypt = ''
 for char in text:
     if 97 <= ord(char) <= 122:
-        crypt += chr((ord(char) + shift - 97) % 26 + 97)
+        crypt += chr((ord(char) + key - 97) % 26 + 97)
     elif 65 <= ord(char) <= 90:
-        crypt += chr((ord(char) + shift - 65) % 26 + 65)
+        crypt += chr((ord(char) + key - 65) % 26 + 65)
+    elif 1072 <= ord(char) <= 1103:
+        crypt += chr((ord(char) + key - 1072) % 32 + 1072)
+    elif 1040 <= ord(char) <= 1071:
+        crypt += chr((ord(char) + key - 1040) % 32 + 1040)
+    else:
+        crypt += char
 print(crypt)
